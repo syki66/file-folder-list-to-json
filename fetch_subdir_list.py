0b0a023ef4be16a312abe9898817a47d62dd284f
syki66 <66syki@gmail.com>
2020-06-18 23:07:53 +0900

    json function finished

--- fetch_subdir_list.py
+++ fetch_subdir_list.py
@@ -2,13 +2,12 @@ import os
 import datetime
 import json
 
-
-user_path = r'D:\' # 사용자의 입력으로 바꾸기
-
+user_path = r'' # 사용자의 입력으로 바꾸기
 
 json_path = "./test.json"
 json_data = {}
-json_data['info'] = []
+json_data['sub_info'] = []
+json_data['root_info'] = []
 
 file_count = 0
 dir_count = 0
@@ -42,47 +41,75 @@ def fetch_sub_things(path):
         # 경로 리스트를 대입해서 json 파일 생성
         make_json(subfile_list)
         make_json(subdir_list)
-
-        '''
-        # 빈 폴더일 경우 디렉토리값을 출력
-        if (not subfile_list): # 파일이 없을경우
-            print(path)
-        elif (not not subfile_list): # 파일이 존재할경우
-            print(subfile_list)
-        else:
-            print("알수 없는 에러")
-
-        '''     
+ 
         # 하위 디렉토리가 존재한다면 재귀함수 호출
         if (not not subdir_list): # 빈리스트가 아니라면 실행
             for subdir in subdir_list:
                 fetch_sub_things(subdir)
 
 
-# 폴더, 파일 정보 알려주는 함수
+# 경로정보를 이용해서 json 만들기
 def make_json(path_list):
     for path in path_list:
-        json_data['info'].append({
+        if (os.path.isdir(path)):
+            path_type = "directory"
+            path_size = get_size(path)
+        elif (os.path.isfile):
+            path_type = "file"
+            path_size = os.path.getsize(path)
+        else:
+            path_type = "unknown"
+
+        json_data['sub_info'].append({
             "path" : path,
-            "create_time" : (os.path.getctime(path)), # 만든시간을 타임 스탬프로 출력 후 실제시간으로 변환
-            "modify_time" : (os.path.getmtime(path)), # 수정시간
-            "access_time" : (os.path.getatime(path)), # 마지막 엑세스시간
-            "file_size"   : os.path.getsize(path) # 파일크기
+            "create_time" : str( datetime.datetime.fromtimestamp( (os.path.getctime(path)) ) ), # 만든시간을 타임 스탬프로 출력 후 실제시간으로 변환
+            "modify_time" : str( datetime.datetime.fromtimestamp( (os.path.getmtime(path)) ) ), # 수정시간
+            "access_time" : str( datetime.datetime.fromtimestamp( (os.path.getatime(path)) ) ), # 마지막 엑세스시간
+            "size"        : path_size, # 크기
+            "type"        : path_type # 폴더인지 파일인지
         })
 
 
-# 결과 보여주는 함수
+# 결과 보여주고 루트 디렉토리 정보 json으로 넣어주는 함수
 def show_result():
-    print(f'루트폴더를 제외한 전체 폴더 수 : {dir_count - 1}') # 루트 폴더를 제외한 폴더 수
-    print(f'전체 파일 수 :{file_count}') # 전체 파일 수
+    json_data['root_info'].append({
+        "path"    : user_path,
+        "folders" : dir_count - 1,
+        "files"   : file_count,
+        "create_time" : str( datetime.datetime.fromtimestamp( (os.path.getctime(user_path)) ) ), # 만든시간을 타임 스탬프로 출력 후 실제시간으로 변환
+        "modify_time" : str( datetime.datetime.fromtimestamp( (os.path.getmtime(user_path)) ) ), # 수정시간
+        "access_time" : str( datetime.datetime.fromtimestamp( (os.path.getatime(user_path)) ) ), # 마지막 엑세스시간
+        "size"        : get_size(user_path), # 크기
+        "type"        : "directory"
+        
+    })
+
+    print(f'위치         : {user_path}')
+    print(f'전체 폴더 수 : {dir_count - 1}') # 루트 폴더를 제외한 폴더 수
+    print(f'전체 파일 수 : {file_count}') # 전체 파일 수
+    print(f'크기         : {get_size(user_path)}')
+    print(f'만든 날짜    : {str( datetime.datetime.fromtimestamp( (os.path.getctime(user_path)) ) )}')
+    print(f'수정한 날짜  : {str( datetime.datetime.fromtimestamp( (os.path.getmtime(user_path)) ) )}')
+    print(f'엑세스 날짜  : {str( datetime.datetime.fromtimestamp( (os.path.getatime(user_path)) ) )}')
+    print(f'타입         : 폴더')
+
+
+# 하위 디렉토리 크기까지 합산해서 현재 디렉토리 크기 계산
+def get_size(path):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            # skip if it is symbolic link
+            if not os.path.islink(fp):
+                total_size += os.path.getsize(fp)
+
+    return total_size
 
 
 fetch_sub_things(user_path)
+show_result()
 
+# json 파일 생성
 with open(json_path, 'w', encoding="utf-8") as outfile:
     json.dump(json_data, outfile, ensure_ascii=False, indent=4)
-
-
-# print(json.dumps(json_data, ensure_ascii=False, indent="\t"))
-
-show_result()
